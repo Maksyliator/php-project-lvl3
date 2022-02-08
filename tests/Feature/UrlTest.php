fb0ca55136e29329ea220fe0985eea3f282bee4e
maksyliator <maksim.tankov@gmail.com>
2022-02-08 08:46:27 +0300

    Fix test

--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -65,36 +65,4 @@ class UrlTest extends TestCase
         $this->post(route('urls.store', ['url' => $newData]));
         $this->assertDatabaseMissing('urls', $newData);
     }
-
-    public function getFixtureFullPath(string $fixtureName): string
-    {
-        $parts = [__DIR__, '../fixtures', $fixtureName];
-        $path = realpath(implode(DIRECTORY_SEPARATOR, $parts));
-        if ($path == false) {
-            throw new \Exception("'/tests/Feature/UrlsChecksTest.php'  Error: path to fextures not found");
-        }
-        return $path;
-    }
-
-    public function testUrlChecks(): void
-    {
-        $pathToFixtures = $this->getFixtureFullPath('htmlTest.html');
-        $body = (string)(file_get_contents($pathToFixtures));
-        if ($body == false) {
-            throw new \Exception("'/tests/Feature/UrlsChecksTest.php'  Error: the file is damaged or missing ");
-        }
-        $checkData = [
-            'url_id' => $this->id,
-            'status_code' => '200',
-            'h1' => 'Новости',
-            'title' => "Форум PHP программистов.",
-            'description' => "Форум PHP программистов."
-        ];
-        Http::fake(fn ($request) => Http::response($body, 200));
-
-        $response = $this->post(route('urls.checks', [$this->id]));
-        $response->assertSessionHasNoErrors();
-        $response->assertRedirect()->assertStatus(302);
-        $this->assertDatabaseHas('url_checks', $checkData);
-    }
 }
